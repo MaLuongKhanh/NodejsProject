fc4d08f9f92f6e9c9b0dbb4b08f7fff4251085ac
MaLuongKhanh <Admin@ADMIN-PC>
2024-10-06 12:14:28 +0700

    update MVC layout

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -10,13 +10,7 @@ class SiteController {
         for (let i = 0; i < arr.length; i++) {
             console.log(arr[i]);
         }
-        res.render('home', {
-            title: 'My Home Page',
-            style: 'style.css',
-            body: '<h1>Welcome to my site!</h1>',
-            header: '<h2>My Header</h2>',
-            footer: '<footer.hbs>My Footer</footer.hbs>',
-        });
+        res.render('home', { arr });
     };
 }
 
