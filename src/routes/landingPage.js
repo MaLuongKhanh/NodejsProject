024122ceb149e95e2a11979756f481d6a7cd416b
MaLuongKhanh <Admin@ADMIN-PC>
2024-11-30 21:45:42 +0700

    fix error name in route path landing page

--- src/routes/landingPage.js
+++ src/routes/landingPage.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const landingPageMiddleWare = require('../app/middlewares/LandingPageMiddleware');
+const landingPageMiddleWare = require('../app/middlewares/LandingPageMiddleWare');
 
 router.get('/', landingPageMiddleWare.index);
 
