13a08e9c031b833846cbcf490993f056b5052d99
MaLuongKhanh <Admin@ADMIN-PC>
2024-11-30 23:27:46 +0700

    add create orders in landing page

--- src/routes/landingPage.js
+++ src/routes/landingPage.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const landingPageMiddleWare = require('../app/middlewares/LandingPageMiddleWare');
+const siteMiddleware = require('../app/middlewares/SiteMiddleWare');
 
 router.get('/', landingPageMiddleWare.index);
+router.post('/yeu-cau-ho-tro', siteMiddleware.createSupportRequest);
 
 module.exports = router;
